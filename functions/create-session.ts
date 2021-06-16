f6f0f76ff22c4b9e98478644847275a31193649b
Clara <claraadora@gmail.com>
2021-06-16 23:40:13 +0800

    Fix all issues

--- functions/create-session.ts
+++ functions/create-session.ts
@@ -1,22 +1,18 @@
 import { adminFirestore } from "./helpers/firebase";
 import { firestore, logger } from "firebase-functions";
 import _ from "lodash";
+import * as admin from "firebase-admin"; 
 
 const TIMESLOTS = ["breakfast", "lunch", "tea"];
 const userCollection = adminFirestore.collection("users");
 const sessionCollection = adminFirestore.collection("sessions");
 
-type TimeslotAndUser = {
-  timeslotToUser: {
-    [timeslot: string]: string[];
-  };
-  userToTimeslot: {
-    [userId: string]: string[];
-  };
+type TimeslotToUser = {
+  [timeslot: string]: string[];
 };
 
 export const createSessionOnCreateUser = firestore
-  .document("users")
+  .document("users/{userId}")
   .onCreate(async () => {
     try {
       const timeslotAndUser = await findAndGroupAvailableUsersPerTimeslot();
@@ -27,101 +23,91 @@ export const createSessionOnCreateUser = firestore
   });
 
 export const createSessionOnUpdateUserAvailability = firestore
-  .document("users")
-  .onUpdate(async (change) => {
+  .document("users/{userId}")
+  .onUpdate(async () => {
     try {
-      // Returns early if `isAvailable` is undefined OR false
-      if (!change.after.data()?.isAvailable) {
-        logger.log("Returns early!")
-        return;
-      }
       const timeslotAndUser = await findAndGroupAvailableUsersPerTimeslot();
       return matchAvailableUsersPerTimeslot(timeslotAndUser);
     } catch (error) {
       logger.warn(error.message);
     }
   });
-  
+
 const findAndGroupAvailableUsersPerTimeslot =
-  async (): Promise<TimeslotAndUser> => {
-    // Two maps for two-way location
+  async (): Promise<TimeslotToUser> => {
     const timeslotToUser = {
       breakfast: [],
       lunch: [],
       tea: [],
     };
-    const userToTimeslot = {};
-
-    // Find available users
-    const availableUsers = adminFirestore
-      .collection("users")
-      .where("isAvailable", "==", false);
 
     // Group available users based on their preferred timeslots
     // Note: users can be in grouped to multiple timeslots
     const promises = TIMESLOTS.map(async (timeslot) => {
       // Find ids of available users who prefer the timeslot
       const availUsersAtTimeslot = (
-        await availableUsers
+        await adminFirestore
+          .collection("users")
+          .where("isAvailable", "==", true)
           .where("preferredTimeslots", "array-contains", timeslot)
           .get()
       ).docs.map((doc) => doc.id);
-
       // Update timeslot to user map
       timeslotToUser[timeslot] = availUsersAtTimeslot;
-
-      // Update user to timeslot map
-      availUsersAtTimeslot.forEach((userId) => {
-        userToTimeslot[userId] = (userToTimeslot[userId] ?? []).concat(
-          timeslot
-        );
-      });
     });
 
     await Promise.all(promises);
 
-    return { timeslotToUser, userToTimeslot };
+    return timeslotToUser;
   };
 
 // For each pair of users in the same timeslot, match them
 // 1. [Firestore] Create an entry in `sessions`
 // 2. [Firestore] Update both users' `isAvailable` status
 // 3. [Local] Remove both users' from other timeslot groups
-const matchAvailableUsersPerTimeslot = async ({
-  timeslotToUser,
-  userToTimeslot,
-}: TimeslotAndUser) => {
+type PairTimeslot = {
+  pair: string[];
+  timeslot: string;
+};
+
+const matchAvailableUsersPerTimeslot = async (
+  timeslotToUser: TimeslotToUser
+) => {
   // Get pairs
-  const pairs: string[][] = [];
-  Object.keys(timeslotToUser).forEach((userIds) => {
-    const timeslotPairs = _.chunk(userIds, 2).filter(
+  const pairTimeslotArr: PairTimeslot[] = [];
+  const pairedUsers: string[] = [];
+
+  Object.entries(timeslotToUser).forEach(([timeslot, userIds]) => {
+    const unpairedUserIds = userIds.filter(
+      (userId) => !pairedUsers.includes(userId)
+    );
+    const timeslotPairs = _.chunk(unpairedUserIds, 2).filter(
       (pair) => pair.length === 2
     );
 
     timeslotPairs.forEach((pair) => {
       // Add each pair to the `pairs`
-      pairs.push(pair);
-      // Remove both users from all of their timeslot groups
-      pair.forEach((user) => {
-        const timeslots = userToTimeslot[user];
-
-        timeslots.forEach((timeslot) => {
-          delete timeslotToUser[timeslot][user];
-        });
-      });
+      pairTimeslotArr.push({ pair, timeslot });
+      // Prevent paired users from participating in the next matching stages
+      pairedUsers.push(pair[0]);
+      pairedUsers.push(pair[1]);
     });
   });
 
-  const promises = pairs.map(async (pair) => {
+  const promises = pairTimeslotArr.map(async ({ pair, timeslot }) => {
     // 1. Create an entry in `sessions`
+    const date = new Date();
+    date.setDate(date.getDate() + 7); 
+
     sessionCollection.add({
       participants: pair,
-      date: Date.now(),
+      date: admin.firestore.Timestamp.fromDate(date),
+      timeslot,
     });
     // 2. Update both users' `isAvailable` status
     pair.forEach((userId) => {
       userCollection.doc(userId).update({
-        isAvailable: true,
+        isAvailable: false,
       });
     });
   });
